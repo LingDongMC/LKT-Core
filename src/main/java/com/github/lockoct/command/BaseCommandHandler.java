df58c32261db986967dbb769a49e5d044a78dbf6
lockoct <825300455@qq.com>
2023-09-30 00:24:43 +0800

    补充提交

--- src/main/java/com/github/lockoct/command/BaseCommandHandler.java
+++ src/main/java/com/github/lockoct/command/BaseCommandHandler.java
@@ -1,9 +1,9 @@
 package com.github.lockoct.command;
 
+import com.github.lockoct.entity.BasePlugin;
+import com.github.lockoct.utils.I18nUtil;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
 
 /**
  * 命令处理类
@@ -11,19 +11,18 @@ import java.util.ArrayList;
  * 既可以将命令在当前类中进行处理，也可以充当路由角色，将子命令分发给对应的子命令处理器进行处理
  */
 public abstract class BaseCommandHandler {
-    protected final ArrayList<String> helpStrList = new ArrayList<>();
 
     // 处理子命令
     public abstract void execute(CommandSender sender, String[] args);
 
     // 输出单条帮助信息
-    public void doHelp(Player player, String helpStr) {
-        player.sendMessage(helpStr);
+    public void doHelpSingle(BasePlugin plugin, Player player, String key) {
+        player.sendMessage(I18nUtil.getText(plugin, player, key));
     }
 
     // 输出多条帮助信息
-    // 常用于输出所有子命令帮助
-    public void doHelp(Player player) {
-        helpStrList.forEach(player::sendMessage);
+    // 常用于输出所有命令帮助
+    public void doHelp(BasePlugin plugin, Player player, String key) {
+        I18nUtil.getTextList(plugin, player, key).forEach(player::sendMessage);
     }
 }
