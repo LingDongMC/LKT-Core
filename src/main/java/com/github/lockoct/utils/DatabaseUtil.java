0b908abec51a2acb1012e62c9dfeb7eca466276a
lockoct <825300455@qq.com>
2025-04-20 00:47:34 +0800

    增加默认数据库功能，初次加载插件时，使用sqlite作为默认数据库

--- src/main/java/com/github/lockoct/utils/DatabaseUtil.java
+++ src/main/java/com/github/lockoct/utils/DatabaseUtil.java
@@ -9,6 +9,8 @@ import org.nutz.dao.impl.NutDao;
 import org.nutz.dao.impl.SimpleDataSource;
 import org.nutz.dao.util.Daos;
 
+import java.io.File;
+
 public class DatabaseUtil {
 
     public static Dao initDB() throws ClassNotFoundException {
@@ -18,7 +20,15 @@ public class DatabaseUtil {
         String username = config.getString("dataSource.username");
         String password = config.getString("dataSource.password");
 
-        if (StringUtils.isNotBlank(driver) && StringUtils.isNotBlank(url) && StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
+        // sqlite，不需要校验用户名密码
+        if (StringUtils.equals(driver, "org.sqlite.JDBC")) {
+            SimpleDataSource dataSource = new SimpleDataSource();
+            dataSource.setDriverClassName(driver);
+            dataSource.setJdbcUrl(url);
+            // 下划线转驼峰
+            Daos.FORCE_HUMP_COLUMN_NAME = true;
+            return new NutDao(dataSource);
+        } else if (StringUtils.isNotBlank(driver) && StringUtils.isNotBlank(url) && StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
             SimpleDataSource dataSource = new SimpleDataSource();
             dataSource.setDriverClassName(driver);
             dataSource.setJdbcUrl(url);
@@ -33,5 +43,31 @@ public class DatabaseUtil {
 
     public static Dao getDao() {
         return Main.plugin.getDao();
+    }
+
+    public static void setDefaultDB() {
+        // 读取配置文件
+        FileConfiguration config = Main.plugin.getConfig();
+        String driver = config.getString("dataSource.driverName");
+        String url = config.getString("dataSource.url");
+
+        // 检查配置文件是否未配置数据库连接信息
+        // 当驱动名称和链接都为空时才使用默认数据库
+        if (StringUtils.isNotBlank(driver) || StringUtils.isNotBlank(url)) {
+            return;
+        }
+
+        String dataFolderPath = Main.plugin.getDataFolder().getAbsolutePath();
+        dataFolderPath = dataFolderPath.replaceAll("\\\\", "/");
+
+        config.set("dataSource.driverName", "org.sqlite.JDBC");
+        config.set("dataSource.url", "jdbc:sqlite:" + dataFolderPath + "/minecraft.db");
+        Main.plugin.saveConfig();
+
+        // 检查是否已存在默认数据库文件
+        File file = new File(dataFolderPath + "/minecraft.db");
+        if (!file.exists()) {
+            Main.plugin.saveResource("minecraft.db", false);
+        }
     }
 }
